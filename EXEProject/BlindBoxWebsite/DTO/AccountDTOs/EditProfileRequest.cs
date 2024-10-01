81333efaa45a84345d4336e82344d7738cf08379
thang109 <144095203+thang109@users.noreply.github.com>
2024-10-01 13:10:02 +0700

    Update User Validate

--- EXEProject/BlindBoxWebsite/DTO/AccountDTOs/EditProfileRequest.cs
+++ EXEProject/BlindBoxWebsite/DTO/AccountDTOs/EditProfileRequest.cs
@@ -4,32 +4,27 @@ namespace BlindBoxWebsite.DTO.AccountDTOs
 {
     public class EditProfileRequest
     {
-        [Required(ErrorMessage = "Email is required.")]
         [StringLength(100, ErrorMessage = "Email cannot be longer than 100 characters.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         [Display(Name = "Email")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Username is required.")]
         [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Username can only contain letters, numbers, and underscores.")]
         [Display(Name = "Username")]
         public string? UserName { get; set; }
 
-        [Required(ErrorMessage = "Full name is required.")]
         [StringLength(100, ErrorMessage = "Full name cannot be longer than 100 characters.")]
-        [RegularExpression(@"^\S.*\S$|^\S$", ErrorMessage = "Full name cannot start or end with whitespace.")]
+        [RegularExpression(@"^(?!\s*$)(?!^\s+|\s+$)[a-zA-Z\s]+$", ErrorMessage = "Full name cannot contain leading or trailing spaces or any numbers.")]
         [Display(Name = "Full Name")]
         public string? FullName { get; set; }
 
-        [Required(ErrorMessage = "Address is required.")]
         [StringLength(200, ErrorMessage = "Address cannot be longer than 200 characters.")]
-        [RegularExpression(@"^\S.*\S$|^\S$", ErrorMessage = "Address cannot start or end with whitespace.")]
+        [RegularExpression(@"^(?!\s*$)(?!^\s+|\s+$).*", ErrorMessage = "Address cannot start or end with whitespace.")]
         [Display(Name = "Address")] 
         public string? Address { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required.")]
         [StringLength(15, ErrorMessage = "Phone number cannot be longer than 15 characters.")]
-        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Phone number must contain only digits.")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Phone number must contain 10 numbers.")]
         [Phone(ErrorMessage = "Invalid phone number format.")]
         [Display(Name = "Phone Number")] 
         public string? PhoneNumber { get; set; }
