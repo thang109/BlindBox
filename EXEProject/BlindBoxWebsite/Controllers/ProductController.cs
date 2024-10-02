85ef589a25fc4156392fd5721c503177ba68bec3
thang109 <144095203+thang109@users.noreply.github.com>
2024-10-02 17:54:27 +0700

    Create ListBlindBox

--- EXEProject/BlindBoxWebsite/Controllers/ProductController.cs
+++ EXEProject/BlindBoxWebsite/Controllers/ProductController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlindBoxWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlindBoxWebsite.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly BlindBoxContext _context;
+
+        public ProductController(BlindBoxContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -30,5 +39,44 @@ namespace BlindBoxWebsite.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> BlindBoxGift()
+        {
+            bool isUserLoggedIn = HttpContext.Session.GetString("UserId") != null;
+            ViewBag.IsUserLoggedIn = isUserLoggedIn;
+            if (isUserLoggedIn)
+            {
+                ViewBag.UserName = HttpContext.Session.GetString("UserName");
+            }
+
+            var blindBoxProducts = await _context.BlindBoxes.ToListAsync();
+
+            return View(blindBoxProducts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadProductImage(IFormFile file, int productId)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var filePath = Path.Combine("wwwroot/images/blindboxes", file.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                var product = await _context.BlindBoxes.FindAsync(productId);
+                if (product != null)
+                {
+                    product.ImageUrl = $"images/blindboxes/{file.FileName}";
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("BlindBoxGift");
+        }
+
     }
 }
